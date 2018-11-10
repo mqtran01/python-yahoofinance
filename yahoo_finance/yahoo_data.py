41d8f6aa9062ae823ad7b147a9619236bff017cc
Michael Tran <michaelqtran01@gmail.com>
2018-11-11 10:26:49 +1100

    safe keeping

--- yahoo_finance/yahoo_data.py
+++ yahoo_finance/yahoo_data.py
@@ -11,6 +11,13 @@ class IYahooData(ABC):
         x: '-' for x in DataFormat._FORMATS
     }
 
+    def __init__(self, stock, locale):
+        if locale == Locale.US:
+            # Special case because the US is special
+            self._base_url = "https://finance.yahoo.com"
+        else:
+            self._base_url = "https://{}.finance.yahoo.com".format(locale)
+
     @abstractmethod
     def to_csv(self, path, line_terminator, sep):
         pass
@@ -21,6 +28,16 @@ class IYahooData(ABC):
             x: '-' for x in DataFormat._FORMATS
         }
         return [heading, ''] + [data.get(index, IYahooData._default_row)[data_fmt] for data in dataset]
+
+    @staticmethod
+    def _fetch_quote_summary(url):
+        html = requests.get(url).text
+        soup = BeautifulSoup(html,'html.parser')
+
+        soup_script = soup.find("script",text=re.compile("root.App.main")).text
+        json_script = json.loads(re.search("root.App.main\s+=\s+(\{.*\})",soup_script)[1])
+        # return json_script
+        return json_script['context']['dispatcher']['stores']['QuoteSummaryStore']
 
 
 class CashFlow(IYahooData):
@@ -59,14 +76,10 @@ class CashFlow(IYahooData):
         }
     )
 
-    def __init__(self, stock):
-        url = 'https://finance.yahoo.com/quote/{}/financials'.format(stock)
-        html = requests.get(url).text
-        soup = BeautifulSoup(html,'html.parser')
-
-        soup_script = soup.find("script",text=re.compile("root.App.main")).text
-        json_script = json.loads(re.search("root.App.main\s+=\s+(\{.*\})",soup_script)[1])
-        fin_data = json_script['context']['dispatcher']['stores']['QuoteSummaryStore']
+    def __init__(self, stock, locale=Locale.US):
+        super().__init__(stock, locale)
+        url = self._base_url + '/{}/financials'.format(stock)
+        fin_data = self._fetch_quote_summary(url)
 
         self.cashflow = self._extract_cashflow(fin_data)
         self.cashflow.sort(key=lambda x: x['endDate']['raw'], reverse=True)
@@ -107,3 +120,48 @@ class CashFlowQuarterly(CashFlow):
     
     def _extract_cashflow(self, fin_data):
         return fin_data['cashflowStatementHistoryQuarterly']['cashflowStatements']
+
+
+class AssetProfile(IYahooData):
+    _info_mapping = {
+        ()
+    }
+
+    _exec_mapping = {
+        ('Name', 'name'),
+        ('Age', 'age'),
+        ('Year Born', 'yearBorn'),
+        ('Exercised Value', 'exercised')
+    }
+
+    def __init__(self, stock, locale=Locale.US):
+        super().__init__(stock, locale)
+
+        url = self._base_url + '/{}/profile'.format(stock)
+        fin_data = self._fetch_quote_summary(url)
+
+        self.profile = fin_data['assetProfile']
+
+    def to_csv(self, path, line_terminator='\n', sep=',', data_format=DataFormat.RAW):
+        with open(path, 'w') as file_handle:
+            csv_handle = csv.writer(file_handle, delimiter=sep)
+
+            csv_handle.writerow(['Profile'])
+            csv_handle.writerow(self._csv_row(self.cashflow, 'Period ending', 'endDate', 'fmt'))
+
+            csv_handle.writerow(self._csv_row(self.cashflow, 'Net income', 'netIncome', data_format))
+
+            for header_mapping in self._table_mapping:
+                csv_handle.writerow([])
+                csv_handle.writerow([header_mapping['header']])
+                for data_mapping in header_mapping['data_map']:
+                    csv_handle.writerow(self._csv_row(self.cashflow, data_mapping[0], data_mapping[1], data_format))
+                    
+            
+            csv_handle.writerow([])
+            # TODO: Find the correct header for this item
+            csv_handle.writerow(self._csv_row(self.cashflow, 'Effect of exchange rate changes', '???', data_format))
+            
+            csv_handle.writerow([])
+            csv_handle.writerow(self._csv_row(self.cashflow, 'Change in cash and cash equivalents', 'changeInCash', data_format))
+
