60f7dbf1799d4e9302fa7d3732728b33cc267cd6
Michael Tran <michaelqtran01@gmail.com>
2018-12-02 12:35:02 +1100

    Added in docs

--- yahoofinance/__init__.py
+++ yahoofinance/__init__.py
@@ -7,3 +7,4 @@ from .cashflow import CashFlow, CashFlowQuarterly
 from .assetprofile import AssetProfile
 from .historicaldata import HistoricalPrices
 from .balancesheet import BalanceSheet, BalanceSheetQuarterly
+from .incomestatement import IncomeStatement, IncomeStatementQuarterly
